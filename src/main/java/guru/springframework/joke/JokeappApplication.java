90b1eac4c2bb84ac6b44107a6c6e622df6d073e0
Prakanshu <raykishanray@gmail.com>
2019-04-10 00:47:57 +0530

    Initial Commit

---
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -0,0 +1,17 @@
+package guru.springframework.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class JokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeappApplication.class, args);
+
+
+    }
+
+}
